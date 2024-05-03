245e5145c448212fd88bc029833af81af930c377
xuhaoran <1710745401@qq.com>
2024-05-03 20:12:57 +0800

    Auto commit by git_upload.sh

---
+++ git_pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 进入到你的 Git 仓库目录
+cd E:/xhr-Remote-restore
+
+# 执行 Git 添加、提交和推送
+git pull origin master
+
+# 输出一条消息表示操作完成
+echo "Git pull complete."
+
+read -p "Press any key to continue..."
