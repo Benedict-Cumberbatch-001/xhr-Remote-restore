8987bdc4ab057a36ff6147d994c6db41244db1a5
xuhaoran <1710745401@qq.com>
2024-05-03 20:04:15 +0800

    Auto commit

---
+++ bjb_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 进入到你的 Git 仓库目录
+cd E:/xhr-Remote-restore
+
+# 执行 Git 添加、提交和推送
+git add .
+git commit -m "Auto commit"
+git push test master
+
+# 输出一条消息表示操作完成
+echo "Git upload complete."
+
+read -p "Press any key to continue..."
