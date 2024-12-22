702fc1dc4a07508be072e168ca5cec576f16784d
xuhaoran <1710745401@qq.com>
2024-12-22 22:10:08 +0800

    Auto commit by git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -5,10 +5,10 @@ cd E:/xhr-Remote-restore
 
 # 执行 Git 添加、提交和推送
 git add .
-git commit -m "Auto commit by git_upload.sh"
+git commit -m "Auto commit by git_push.sh"
 git push origin master
 
 # 输出一条消息表示操作完成
-echo "Git upload complete."
+echo "Git push complete."
 
 read -p "Press any key to continue..."
